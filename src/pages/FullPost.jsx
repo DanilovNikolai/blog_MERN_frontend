b7bcb83dd2676b2c72a66bd26e120e23c2e73475
Danilov_Nikolai <dan1k91@mail.ru>
2024-10-07 00:11:25 +0300

    CommentsBlock visibility fixed

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -70,7 +70,7 @@ export const FullPost = () => {
       >
         <ReactMarkdown children={postsData.text} />
       </Post>
-      {comments && (
+      {comments.length && (
         <CommentsBlock
           comments={filteredComments} // Фильтруем комментарии по postId
           postId={params.id}
